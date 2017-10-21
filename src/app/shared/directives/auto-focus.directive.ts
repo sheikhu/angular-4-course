ff3f6389985d497b0b814be723355aac70c05bd5
Sheikhu <sheikhu02@gmail.com>
2017-10-21 18:22:24 +0000

    Course Project - Directives

---
+++ src/app/shared/directives/auto-focus.directive.ts
@@ -0,0 +1,14 @@
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appAutoFocus]'
+})
+export class AutoFocusDirective implements OnInit {
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+  this.elementRef.nativeElement.focus();
+  }
+
+}
---
+++ src/app/shared/directives/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
+/**
+ * Created by sheikhu on 21/10/17.
+ */
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit {
+  @HostBinding('class.show') opened = false;
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.opened = !this.opened;
+  }
+}
